96eacdb1b2391183e0229547bb745ed4f234e516
Thytu <valentin.de-matos@epitech.eu>
2021-10-01 17:17:08 +0200

    refactor: normalization now cast tensors to device

--- normalization.py
+++ normalization.py
@@ -1,17 +1,20 @@
 import torch
+from torch._C import device
 import torch.nn as nn
 
 # create a module to normalize input image so we can easily put it in a
 # nn.Sequential
 class Normalization(nn.Module):
-    def __init__(self, mean, std):
+    def __init__(self, mean, std, device):
+        self.device = device
+
         super(Normalization, self).__init__()
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
+        self.mean = torch.tensor(mean).view(-1, 1, 1).to(device)
+        self.std = torch.tensor(std).view(-1, 1, 1).to(device)
 
     def forward(self, img):
         # normalize img
-        return (img - self.mean) / self.std
+        return (img.to(self.device) - self.mean) / self.std
