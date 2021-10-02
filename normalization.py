cd7f254ecee0ae29262e9c6c41fefa787b7f6dcd
Thytu <valentin.de-matos@epitech.eu>
2021-10-02 16:41:07 +0200

    refactor: normalization no longer produce warning by using .clone().detach()

--- normalization.py
+++ normalization.py
@@ -1,20 +1,18 @@
-import torch
-from torch._C import device
 import torch.nn as nn
 
-# create a module to normalize input image so we can easily put it in a
-# nn.Sequential
+# Create a module to normalize input image so we can easily put it in a nn.Sequential
 class Normalization(nn.Module):
     def __init__(self, mean, std, device):
+        super(Normalization, self).__init__()
+
         self.device = device
 
-        super(Normalization, self).__init__()
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1).to(device)
-        self.std = torch.tensor(std).view(-1, 1, 1).to(device)
+
+        self.mean = mean.clone().detach().view(-1, 1, 1).to(device)
+        self.std = std.clone().detach().view(-1, 1, 1).to(device)
 
     def forward(self, img):
-        # normalize img
         return (img.to(self.device) - self.mean) / self.std
